cc68392c71334a3e4a1fcfad9d77c6f006b7868d
Terrence Ho <terrenceho.books@gmail.com>
2019-02-16 09:55:49 -0800

    First Commit

---
+++ backend/data.py
@@ -0,0 +1,6 @@
+data = {
+
+}
+
+def load():
+    pass
---
+++ backend/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/federal/geographic')
+def federal_geographic():
+    pass
+
+@app.route('/federal/issue')
+def federal_issue():
+    pass
+
+@app.route('/state/geographic')
+def state_geographic():
+    pass
+
+@app.route('local/temporal')
+def local_temporal():
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
